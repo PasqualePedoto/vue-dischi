7b557d2b027664259c531d2e6cd2318f96832a24
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-07-27 14:35:43 +0200

    Creato ed aggiunto il nuovo componente BaseSelect in BaseHeader

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,20 +1,27 @@
 <template>
-  <section class="container p-0">
+  <section class="p-0 container">
     <header>
       <figure class="m-0"><img class="img-fluid m-3" src="../assets/image/logo.svg" alt="spotify-logo" /></figure>
+      <BaseSelect />
     </header>
   </section>
 </template>
 
 <script>
+import BaseSelect from "./BaseSelect.vue";
 export default {
   name: "BaseHeader",
+  components: { BaseSelect },
 };
 </script>
 
 <style scoped lang="scss">
 header {
   background-color: #2e3a46;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-right: 20px;
 
   figure {
     width: 150px;
---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,21 @@
+<template>
+  <select class="form-select" aria-label="Default select example">
+    <option>-- --</option>
+    <option value="1" selected>One</option>
+    <option value="2">Two</option>
+    <option value="3">Three</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "BaseHeader",
+};
+</script>
+
+<style lang="scss" scoped>
+select {
+  width: 200px;
+  cursor: pointer;
+}
+</style>
